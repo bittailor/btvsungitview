7187bcc48d97213c3cfb688e57e8c54906201bf7
Franz Schnyder <schnyderfranz@gmail.com>
2018-01-23 23:02:39 +0100

    Adapt to show Ungit in IFrame

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,25 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const scheme = "btungit";
+const previewUri = vscode.Uri.parse(`${scheme}://view`);
+
+class UngitViewProvider implements vscode.TextDocumentContentProvider {
+    
+    onDidChange?: vscode.Event<vscode.Uri>;
+    
+    provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
+        const location = vscode.workspace.rootPath || "";
+        const url = `http://localhost:8448/#/repository?path=${location}`;
+        return `
+        <div style="position: fixed; height: 100%; width: 100%; margin-left: -20px;">
+            <iframe src="${url}" style="border: none;" height="100%" width="100%"></iframe>
+        </div>
+        `;
+    }
+    
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -14,12 +33,17 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let disposable = vscode.commands.registerCommand('extension.btvgit', () => {
+        vscode.commands.executeCommand("vscode.previewHtml", previewUri, vscode.ViewColumn.Two, "Git View").then(() => {
+            return;
+        }, (reason: string) => {
+            vscode.window.showErrorMessage(reason);
+        });
     });
+
+    let provider = new UngitViewProvider();
+
+    vscode.workspace.registerTextDocumentContentProvider(scheme, provider);
 
     context.subscriptions.push(disposable);
 }
